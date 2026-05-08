b39ba12ca076755d4ab73b8a9515ca229a22fe41
Nitesh <niteshemailxyz@mail.com>
2026-05-09 02:46:52 +0530

    Add Todo Logic and Styling

--- 03-todo-website/script.js
+++ 03-todo-website/script.js
@@ -1,22 +1,60 @@
-const todoList = document.getElementById('todo-lists')
-const form = document.getElementById('todo-form');
-const input = document.getElementById('todo')
-let todos = []
+const todoForm = document.getElementById('todo-form');
+const inputTodo = document.getElementById('input-todo');
+const todosList = document.getElementById('todos-list');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+const key = 'localStorageTodos'         // I'm Greatest Coder I thougth why not to name it as key to access local storage
+let todos = [];
 
-    const newTodo = input.value;
+// Fetch All Todos From LocalStorage and Set into todos array on window load
+window.addEventListener('load', fetchLocalTodos);
 
-    console.log('Todo: ', newTodo)
+// Run On Submit Button
+todoForm.addEventListener('submit', (event) => {
+    event.preventDefault();     // Prevent Reload I'm Not Using Ai i forget everytime so I Addded this comment here!!!
+    
+    const inputTodoValue = inputTodo.value.trim();
 
-    todos.push(newTodo)
+    if(inputTodoValue !== '') {
+        todos.push(inputTodoValue);
+        saveTodos();
+        renderTodos();
+        inputTodo.value = '';
+    }
+});
 
-    const liTodo = document.createElement('li')
-    liTodo.textContent = newTodo
-    todoList.appendChild(liTodo)
 
-    localStorage.setItem('myTodos', todos)
+function saveTodos() {
+    localStorage.setItem(key, JSON.stringify(todos));
+}
 
-    input.value = ''
-} )
+
+function fetchLocalTodos() {
+    const saveTodos = localStorage.getItem(key);
+    if(saveTodos) {
+        todos = JSON.parse(saveTodos);
+        renderTodos();
+    }
+}
+
+function renderTodos() {
+    todosList.innerHTML = ''
+
+    todos.forEach((todoText, index) => {
+        const newTodoList = document.createElement('li');
+        newTodoList.innerHTML = `<span>${todoText}</span>`;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+
+        deleteBtn.addEventListener('click', () => {
+            todos.splice(index, 1);
+
+            saveTodos();
+
+            renderTodos();
+        })
+
+        newTodoList.appendChild(deleteBtn);
+        todosList.appendChild(newTodoList);
+    })
+}
